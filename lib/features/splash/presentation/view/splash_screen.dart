d737db72d7b53e6bda1c44c64b6775197c72a26b
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2025-10-17 21:18:31 +0200

    Create Splash Screen And Add Some Packages

---
+++ lib/features/splash/presentation/view/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  static String routeName ="SplashScreen";
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3),(){
+      // Navigator.pushNamed(context, HomeScreen.routeName);
+    });  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(
+          height:MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+        ),
+        child: Image.asset("assets/images/Splash Screen.png",
+            fit: BoxFit.fill),
+      ),
+    );
+  }
+}
